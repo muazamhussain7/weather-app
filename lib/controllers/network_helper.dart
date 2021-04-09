c0d21c7a92e38aaf6facb291a0a5b0a7aa30fa85
Muazam Hussain <81756944+muazamhussain7@users.noreply.github.com>
2021-04-09 19:47:16 +0500

    modified network_helper.dart file for security purposes.

--- lib/controllers/network_helper.dart
+++ lib/controllers/network_helper.dart
@@ -7,7 +7,7 @@ class NetworkHelper {
   static const _APIURL = 'api.openweathermap.org';
   static const _APIROUTE = '/data/2.5/group';
   static const urlForWeatherIcon = 'http://openweathermap.org/img/w/';
-  static const _APIKEY = 'abae32df2626399b7bb908d1648361b2';
+  static const _APIKEY = 'Your API Key';
   static const _citiesId =
       '1172451,1174872,1177662,1162015,3117735,1166548,1167528,1169684,1169825,2643743';
   static const _temperatureUnit = 'metric';
